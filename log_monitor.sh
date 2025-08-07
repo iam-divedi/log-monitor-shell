045cd6c386ce224caac738f77d36b32cd6f443e6
iam-divedi <69568648+iam-divedi@users.noreply.github.com>
2025-08-07 23:45:58 +0530

    Update log_monitor.sh

--- log_monitor.sh
+++ log_monitor.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-# Ye wo file hai jisme tu errors dhundega
+
 LOG_FILE="/var/log/syslog"
 
-# Ye keywords hain jinpe alert milega
+
 KEYWORDS=("ERROR" "CRITICAL" "FAILURE")
 
-# Jisme tu matched logs store karega
+
 OUTPUT_LOG="./log_summary.txt"
 
-# Alert log file
 ALERT_LOG="./alert_triggered.txt"
 
 echo "🟡 Starting Log Monitoring..."
 echo "Scanning: $LOG_FILE"
 echo "---------------------------------------------------"
 
-# Har line read karte jao aur keyword se match karo
+
 while read -r line; do
     for keyword in "${KEYWORDS[@]}"; do
         if echo "$line" | grep -q "$keyword"; then
